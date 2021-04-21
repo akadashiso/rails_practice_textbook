519e5c9aaf79c90dfe7d24d81800cfac61950ef8
akadashiso <akadashiso@gmail.com>
2021-04-21 14:31:01 +0000

    [Start]1-3 4-2当てはまらない場合に分岐するunless

---
+++ ruby/chapter1/1-3.rb
@@ -0,0 +1,44 @@
+age = 20
+
+unless age >= 20
+  puts "未青年には酒類を提供できません！"
+else
+  puts "おすすめのワインがあります。"
+end
+
+
+# age = 16
+# unless age >= 20
+#   puts "未青年には酒類を提供できません！"
+# end
+
+
+# # -----------------
+# number = 3
+# message = if number > 50
+#             "numberは50より大きいです"
+#           else
+#             "numberは50以下です"
+#           end
+
+# puts message
+
+# -----------------
+# if number == 1
+#   puts '数値は１です'
+# elsif number == 2
+#   puts '数値は2です'
+# else
+#   puts '数値は3です'
+# end
+
+# -----------------
+# result = 1 + 2 * 4 / 8
+
+# puts result
+
+# value = 2
+
+# if value == 3
+#   puts value
+# end
